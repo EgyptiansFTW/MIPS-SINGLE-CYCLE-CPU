24a457897b947b7dad84082c7e4927ea2aa61f72
Mark Attia <mark.attia.145@my.csun.edu>
2022-11-29 01:46:31 -0800

    half of ALU_TB

--- MIPS_SINGLE_CYCLE_CPU.srcs/sim_1/new/ALU_TB.vhd
+++ MIPS_SINGLE_CYCLE_CPU.srcs/sim_1/new/ALU_TB.vhd
@@ -30,37 +30,46 @@ UUT: entity work.ALU port map (OPCODE => OPCODE_tb, SHAMT => SHAMT_tb, X => X_tb
 
 process
 begin 
+--add
 OPCODE_tb <= "0001";
 X_tb <= X"00000005";
 Y_tb<= X"00000005";
 wait for 20 ns;
+--sub
 OPCODE_tb <= "0010";
 X_tb <= X"00000010";
 Y_tb<= X"00000006";
 wait for 20 ns;
+--mul
 OPCODE_tb <= "0011";
 X_tb <= X"11111111";
 Y_tb<= X"11111111";
 wait for 20 ns;
+--div
 OPCODE_tb <= "0100";
 X_tb <= X"0000000A";
 Y_tb<= X"00000003";
+--AND
 OPCODE_tb <= "0101";
 X_tb <= X"00000004";
 Y_tb<= X"00000004";
 wait for 20 ns;
+--OR
 OPCODE_tb <= "0110";
 X_tb <= X"00000008";
 Y_tb<= X"00000002";
 wait for 20 ns;
+--NOR
 OPCODE_tb <= "0111";
 X_tb <= X"FFFFFFFF";
 Y_tb<= X"00000000";
 wait for 20 ns;
+--XOR
 OPCODE_tb <= "1000";
 X_tb <= X"FFFFFFFF";
 Y_tb<= X"00000000";
 wait for 20 ns;
+--SET IF LESS THAN
 OPCODE_tb <= "1101";
 X_tb <= X"00000002";
 Y_tb<= X"00000003";
